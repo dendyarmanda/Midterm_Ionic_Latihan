3925f4e712c794bb47951d94bdf86ee96c1aba01
Dendy Septian Armanda <dendyseptianarmanda@gmail.com>
2022-11-01 16:10:22 +0700

    Edit Home

--- MidtermLatihan/src/app/home/home.page.ts
+++ MidtermLatihan/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { MyModalComponent } from './../components/my-modal/my-modal.component';
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
+   async openModal(){
+      const modal = await this.modalCtrl.create({
+        component:MyModalComponent
+      });
+      modal.present();
 
+      const {data,role} =  await modal.onWillDismiss();
+
+      console.log("Data ", data);
+      console.log("Role ", role);
+
+   }
 }
